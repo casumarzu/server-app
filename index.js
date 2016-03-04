50cd77599ebbb8ef88ac9bf01940531023c9842a
andrey <andrey.ivanov@saltpepper.ru>
2016-03-04 14:25:47 +0300

    Add vhost

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-// const express = require('express');
-// const app = express();
-//
-// app.set('port', (process.env.PORT || 5000));
-//
-// app.use(express.static(__dirname + '/public'));
-//
-// // views is directory for all template files
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
-//
-// app.get('/', function(request, response) {
-//   response.render('pages/index');
-// });
-//
-// app.listen(app.get('port'), function() {
-//   console.log('Node app is running on port', app.get('port'));
-// });
-
 const koa = require('koa');
 const serve = require('koa-static-folder');
 const router = require('koa-route');
@@ -40,16 +21,86 @@ const jade = new Jade({
   //   { random: 'path/to/lib/random.js' },
   //   { _: require('lodash') }
   // ],
-  app: app // equals to jade.use(app) and app.use(jade.middleware)
+  // app: app // equals to jade.use(app) and app.use(jade.middleware)
 })
 
+app.use(jade.middleware);
 app.use(serve(path.join(__dirname, 'public')));
 
-
-app.use(function * (next) {
+app.use(router.get('/', function* () {
   this.render('index');
-  // yield this.render('index.jade');
+}));
+
+app.use(router.get('/foo', function* () {
+  this.body = 'foo';
+}));
+
+app.use(router.get('/bar', function* () {
+  this.body = 'bar';
+}));
+
+// server 1
+var server1 = koa();
+server1.use(jade.middleware);
+
+server1.use(router.get('/', function* () {
+  this.render('index');
+}));
+
+server1.use(router.get('/foo', function* () {
+  this.body = 'foo';
+}));
+server1.use(router.get('/bar', function* () {
+  this.body = 'bar';
+}));
+// server 1
+
+
+// server 2
+var server2 = koa();
+server2.use(jade.middleware);
+server2.use(function *(next) {
+  this.render('index');
 });
+server2.use(router.get('/', function* () {
+  this.render('index');
+}));
+
+server2.use(router.get('/foo', function* () {
+  this.body = 'foo';
+}));
+server2.use(router.get('/bar', function* () {
+  this.body = 'bar';
+}));
+// server 2
+
+
+// server 3
+var server3 = koa();
+server3.use(jade.middleware);
+server3.use(function *(next) {
+  this.render('index');
+});
+server3.use(router.get('/', function* () {
+  this.render('index');
+}));
+
+server3.use(router.get('/foo', function* () {
+  this.body = 'foo';
+}));
+server3.use(router.get('/bar', function* () {
+  this.body = 'bar';
+}));
+// server 3
+
+app.use(
+  vhost([
+    { host: 's1.example.com', app: server1 },
+    { host: 's2.example.com', app: server2 },
+    { host: 's3.example.com', app: server3 },
+  ])
+);
+
 
 app.listen(port, function() {
   console.log('server listening port: ' + port);
